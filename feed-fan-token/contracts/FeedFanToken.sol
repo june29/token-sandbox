58b1f8ac7123245ef380788d673d4ad6787db16e
june29 <june29.jp@gmail.com>
2022-10-11 00:05:11 +0900

    こんな感じ…？

--- feed-fan-token/contracts/FeedFanToken.sol
+++ feed-fan-token/contracts/FeedFanToken.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.8.9;
 
 contract FeedFanToken {
-  
+  mapping(uint256 => string) public channelUrls; // Token ID => Channel URL
+  mapping(uint256 => string[]) public consumedItems; // Token ID => Consumed Items  
 }
